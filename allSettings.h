752c04bdd3c7a712d4f268d78273d9ef87c22266
TechnoPhreak <marc.guy.verbard@gmail.com>
2024-10-30 15:31:53 -0400

    Update: Add WiFi Settings

--- allSettings.h
+++ allSettings.h
@@ -3,6 +3,10 @@
 const String allSettings = R"(
   {
     "device_name": { "type": "string", "default": "Untitled" },
+    "wifi_ssid": { "type": "string", "default": "" },
+    "wifi_password": { "type": "string", "default": "" },
+    "wifi_timeout": { "type": "int", "default": 10 },
+    "wifi_dhcp": { "type": "boolean", "default": true },
   }
 )";
 
