c15ab1c374a951643d7a4dfe8d81dac13f11e6b8
TechnoPhreak <marc.guy.verbard@gmail.com>
2024-10-30 16:33:23 -0400

    Update: Add a couple more base code for WiFi , OTA and Rest

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # openDryBox
 A versatile Filament Dry Box projet for 3D Printing enthusiasts
+
+## Dependencies
+* ArduinoJSON 7.2.0
--- openDryBox.ino
+++ openDryBox.ino
@@ -1,5 +1,16 @@
+#include <Preferences.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+#include <mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 const char* programName = "openDryBox";
 const char* programVersion = "v0.0.1";
+
+const char* ota_password = "@rduin0";
+bool otaServiceStarted = 0;
 
 // Load Settings Definition
 #include "allSettings.h"
