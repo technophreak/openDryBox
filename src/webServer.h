ee93b905ca707fa02999bac04fa7c0f16efbd1e7
TechnoPhreak <marc.guy.bernard@gmail.com>
2025-08-31 19:15:16 -0400

    Reorganizes project structure for VSCode and PlatformIO
    Restructures directories and files to enhance organization and compatibility with Visual Studio Code and PlatformIO.

---
+++ src/webServer.h
@@ -0,0 +1,33 @@
+#ifndef _ODB_WEBSERVER_H_
+#define _ODB_WEBSERVER_H_
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+#include <ESP32WebServer.h>
+#include <ESPmDNS.h>
+
+#include "allSettings.h"
+
+class WebServer {
+  public:
+    WebServer(JsonObject objSettings, Preferences* myPreferences);
+    ~WebServer();
+    void handleClient();
+  private:
+    void initRoutes();
+    void getHomePage();
+    void getJsonStatus();
+    void getJsonSettings();
+    void setSettings();
+    void otaStart();
+    void otaStop();
+    void handleNotFound();
+    void espRestart();
+
+    ESP32WebServer* restServer;
+    JsonObject objSettings;
+    Preferences* myPreferences;
+};
+
+#endif
